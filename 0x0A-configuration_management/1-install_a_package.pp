56739e3e32b4dbe376f13d21e9b03a394ec56f7f
SamuelAmiaka <amiakasamuel010@gmail.com>
2023-01-12 06:23:51 -0800

    install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,6 @@
-able File  6 lines (5 sloc)  95 Bytes
-
 # Installs puppet-lint
 
-package { 'puppet-lint':
-  ensure   => '2.5.1',
-  provider => 'gem',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
